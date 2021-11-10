b05a6d895ac5a9dc4e650c74be245116dacd1d43
QuintenMuyllaert <quinten.muyllaert@student.howest.be>
2021-11-10 10:08:46 +0100

    Added Get method

--- RS3/RS3/Models/HTTP.cs
+++ RS3/RS3/Models/HTTP.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RS3.Models
 {
     internal class HTTP
     {
+        public static async Task<string> Get(string uri)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return string.Empty;
+            }
+        }
     }
 }
