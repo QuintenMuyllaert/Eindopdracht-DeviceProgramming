b9c689d01ea447b409bea716bc0889fa0211c0ef
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2021-12-19 22:20:31 +0100

    PUT & POST overrides

--- API/index.js
+++ API/index.js
@@ -6,16 +6,26 @@ let override = {
   16687466: null,
 };
 
+let addition = {};
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 app.get("/item/*", async (req, res) => {
   console.log("get item", req.path);
+  const itemId = req.path.split("/")[2];
   try {
     let data = await got(`https://api.portcalc.website${req.path}`);
     let replyJSON = JSON.parse(data.body);
 
     let ids = Object.keys(override);
+    console.log(itemId, addition[itemId]);
+    if (addition[itemId]) {
+      for (let add of addition[itemId]) {
+        replyJSON.tran.unshift(override[add]);
+      }
+    }
+
     for (let i = 0; i < replyJSON.tran.length; i++) {
       if (ids.includes(replyJSON.tran[i].id)) {
         if (override[replyJSON.tran[i].id] == null) {
@@ -53,13 +63,23 @@ app.delete("*", async (req, res) => {
 
 app.put("*", async (req, res) => {
   //edit existing transaction
-  console.log("PUT!",req.body);
+  console.log("PUT!", req.body);
+  override[req.body.id] = req.body;
   res.end();
 });
 
 app.post("*", async (req, res) => {
   //create new transaction
-  console.log("POST!",req.body);
+  console.log("POST!", req.body);
+  req.body.id = Date.now();
+
+  override[req.body.id] = req.body;
+
+  if (addition[req.body.ItemId]) {
+    addition[req.body.ItemId].push(req.body.id);
+  } else {
+    addition[req.body.ItemId] = [req.body.id];
+  }
   res.end();
 });
 
