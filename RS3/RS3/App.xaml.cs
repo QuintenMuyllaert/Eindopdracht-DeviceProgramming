d4e2a1de03b7ec54af97a3a6a822f9a0bf646ada
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2021-12-19 21:54:22 +0100

    No internet warning

--- RS3/RS3/App.xaml.cs
+++ RS3/RS3/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RS3.Views;
+using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,8 +12,31 @@ namespace RS3
         {
             InitializeComponent();
 
+            // Register for connectivity changes, be sure to unsubscribe when finished
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+
+            void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+            {
+                var access = e.NetworkAccess;
+                var profiles = e.ConnectionProfiles;
+                var current = Connectivity.NetworkAccess;
+
+                if (current == NetworkAccess.Internet)
+                {
+                    // Connection to internet is available
+                    MainPage = new NavigationPage(new NavigationPage(new MainPage()));
+                }
+                else
+                {
+                    MainPage = new NavigationPage(new NavigationPage(new NoWifi()));
+                }
+            }
+
+
             MainPage = new NavigationPage(new MainPage());//Very important add this.. R.I.P. HOURS
             //MainPage = new MainPage();//grrr.exe
+
+
 
         }
 
